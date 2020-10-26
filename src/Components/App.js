cbb3ae3d94955ce3cb1b33d7828a3e58b3ff939a
omarvegaabreu <omarvega@hotmail.com>
2020-10-25 20:29:04 -0400

    changed call to async await

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,17 +5,23 @@ import Images from "./Images";
 import { API_KEY, API_URL } from "../util/apikeys";
 
 class App extends React.Component {
-  onSearchSubmit = (term) => {
-    axios.get(API_URL(), {
+  state = {
+    images: "",
+  };
+
+  async onSearchSubmit(term) {
+    const response = await axios.get(API_URL(), {
       params: { query: term },
       headers: {
         Authorization: `Client-ID ${API_KEY()}`,
       },
     });
-  };
+    console.log(response.data.results);
+  }
   render() {
     return (
       <div className="ui container">
+        <div>{`images: ${this.state.images}`}</div>
         <SearchBar onSubmit={this.onSearchSubmit} />
         <Images />
       </div>
