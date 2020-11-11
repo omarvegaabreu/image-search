62d7e237d718a7ac788e3f9159c3ea9df0d3468e
omarvegaabreu <omarvega@hotmail.com>
2020-11-11 10:10:21 -0500

    refactor

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -3,17 +3,15 @@ import React from "react";
 import ImageCard from "./ImageCard";
 
 const Images = (props) => {
+  console.log("props " + props);
   // <Image src={imgUrl} alt="alt" size="medium" bordered key={imgUrl} />;
 
-  const renderToScreen = props.children.map((images) => {
-    const { description, id, urls } = images;
-    //images object contains all info from the API
+  const renderToScreen = props.children.map(({ description, id, urls }) => {
+    const imageDescription = description; //image description
+    const imageId = id;
+    const regularImage = urls.regular;
 
-    return (
-      <div key={id}>
-        <ImageCard alt={description} src={urls.regular} />
-      </div>
-    );
+    return <ImageCard alt={imageId} />;
   });
 
   return <div className="image-grid">{renderToScreen}</div>;
